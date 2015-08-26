b6f9999970f8a81aa52c808dfd948022d2a197ba
Jérôme Odier <odier.jerome@gmail.com>
2015-08-26 13:48:24 +0200

    update

--- AMIJavaCore/src/main/java/net/hep/ami/utility/Ssh.java
+++ AMIJavaCore/src/main/java/net/hep/ami/utility/Ssh.java
@@ -12,11 +12,11 @@ public class Ssh extends ShellAbstractClass
 
 	/*---------------------------------------------------------------------*/
 
-	public Ssh(String host, String user, String password) throws Exception
+	public Ssh(String host, int port, String user, String password) throws Exception
 	{
 		m_jsch = new JSch();
 
-		m_session = m_jsch.getSession(user, host, 22);
+		m_session = m_jsch.getSession(user, host, port);
 
 		m_session.setPassword(password);
 	}
