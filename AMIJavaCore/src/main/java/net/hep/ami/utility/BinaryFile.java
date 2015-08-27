b43b11810e0a27391721d546a9bb6de9999c4935
Jérôme Odier <odier.jerome@gmail.com>
2015-08-27 13:10:15 +0200

    update

---
+++ AMIJavaCore/src/main/java/net/hep/ami/utility/BinaryFile.java
@@ -0,0 +1,20 @@
+package net.hep.ami.utility;
+
+public class BinaryFile
+{
+	/*---------------------------------------------------------------------*/
+
+	public static void read(/* TODO */) throws Exception
+	{
+		/* TODO */
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static void write(/* TODO */) throws Exception
+	{
+		/* TODO */
+	}
+
+	/*---------------------------------------------------------------------*/
+}
