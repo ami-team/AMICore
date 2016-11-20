0eb52f2322597e98266163a4fd43f7d6791b8884
Jérôme Odier <odier.jerome@gmail.com>
2016-11-20 16:27:21 +0100

    update

---
+++ AMIJavaCore/src/main/java/net/hep/ami/command/admin/RemoveUser.java
@@ -0,0 +1,64 @@
+package net.hep.ami.command.admin;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class RemoveUser extends CommandAbstractClass
+{
+	/*---------------------------------------------------------------------*/
+
+	public RemoveUser(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String amiLogin = arguments.get("amiLogin");
+
+		if(amiLogin == null)
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		TransactionalQuerier transactionalQuerier = getQuerier("self");
+
+		/*-----------------------------------------------------------------*/
+
+		String sql = String.format("DELETE FROM `router_user` WHERE `AMIUser`='%s'",
+			amiLogin.replace("'", "''")
+		);
+
+		int nb = transactionalQuerier.executeUpdate(sql);
+
+		/*-----------------------------------------------------------------*/
+
+		return new StringBuilder(
+			nb > 0 ? "<info><![CDATA[done with success]]></info>"
+			       : "<error><![CDATA[nothing done]]></error>"
+		);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Remove user.";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "-amiLogin=\"value\"";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
