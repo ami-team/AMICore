c96cd5c5d0db06c8911c9fc9eb5c84b46ccd0b8f
Jérôme Odier <odier.jerome@gmail.com>
2016-12-07 11:05:00 +0100

    update

--- OracleMaven.sh
+++ OracleMaven.sh
@@ -5,5 +5,5 @@ JAR_VERSION=12.1.0.1.0
 
 mvn install:install-file \
 	-DgeneratePom=true \
-	-DgroupId=com.oracle -DartifactId=ojdbc7 \
+	-DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 \
 	-Dpackaging=jar -Dfile=$JAR -Dversion=$JAR_VERSION
