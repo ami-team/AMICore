94b0b90c5f7eadd75777183c3eebf533e52ba0ab
Jérôme Odier <odier.jerome@gmail.com>
2017-04-28 08:48:13 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/utility/parser/CSV.java
@@ -0,0 +1,188 @@
+package net.hep.ami.utility.parser;
+
+import java.io.*;
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class CSV
+{
+	/*---------------------------------------------------------------------*/
+
+	public static List<Map<String, String>> parseAsMap(String s) throws Exception
+	{
+		return parseAsMap(CharStreams.fromString(s));
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static List<List<String>> parseAsList(String s) throws Exception
+	{
+		return parseAsList(CharStreams.fromString(s));
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static List<Map<String, String>> parseAsMap(InputStream inputStream) throws Exception
+	{
+		return parseAsMap(CharStreams.fromStream(inputStream));
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static List<List<String>> parseAsList(InputStream inputStream) throws Exception
+	{
+		return parseAsList(CharStreams.fromStream(inputStream));
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static List<Map<String, String>> parseAsMap(CharStream charStream) throws Exception
+	{
+		List<Map<String, String>> result = new ArrayList<>();
+
+		/*-----------------------------------------------------------------*/
+
+		List<List<String>> list = parseAsList(charStream);
+
+		/*-----------------------------------------------------------------*/
+
+		Map<String, String> map;
+
+		if(list.isEmpty() == false)
+		{
+			final int nb1 = list.size();
+			final int nb2 = list.get(0).size();
+
+			for(int i = 1; i < nb1; i++)
+			{
+				/*---------------------------------------------------------*/
+
+				if(list.get(i).size() != nb2)
+				{
+					throw new Exception("corrupted CSV file");
+				}
+
+				/*---------------------------------------------------------*/
+
+				map = new LinkedHashMap<>();
+
+				for(int j = 0; j < nb2; j++)
+				{
+					map.put(
+						list.get(0).get(j)
+						,
+						list.get(i).get(j)
+					);
+				}
+
+				result.add(map);
+
+				/*---------------------------------------------------------*/
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return result;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static List<List<String>> parseAsList(CharStream charStream) throws Exception
+	{
+		/*-----------------------------------------------------------------*/
+
+		CSVParser parser = new CSVParser(new CommonTokenStream(new CSVLexer(charStream)));
+
+		/*-----------------------------------------------------------------*/
+
+		parser.setErrorHandler(new DefaultErrorStrategy() {
+		});
+
+		/*-----------------------------------------------------------------*/
+
+		return visitFile(parser.file());
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private static List<List<String>> visitFile(CSVParser.FileContext ctx)
+	{
+		List<List<String>> result = new ArrayList<>();
+
+		/*-----------------------------------------------------------------*/
+
+		ParseTree child;
+
+		final int nb = ctx.getChildCount();
+
+		for(int i = 0; i < nb; i++)
+		{
+			child = ctx.getChild(i);
+
+			if(child instanceof CSVParser.RowContext)
+			{
+				result.add(visitRow((CSVParser.RowContext) child));
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return result;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private static List<String> visitRow(CSVParser.RowContext ctx)
+	{
+		List<String> result = new ArrayList<>();
+
+		/*-----------------------------------------------------------------*/
+
+		ParseTree child;
+
+		final int nb = ctx.getChildCount();
+
+		for(int i = 0; i < nb; i++)
+		{
+			child = ctx.getChild(i);
+
+			if(child instanceof CSVParser.FieldContext)
+			{
+				result.add(visitField((CSVParser.FieldContext) child));
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return result;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private static String visitField(CSVParser.FieldContext ctx)
+	{
+		String result = ctx.getText();
+
+		/**/ if(result.charAt(0) == '\"')
+		{
+			result = Unescape.unescape(
+				result.substring(1, result.length() - 1)
+			).replace("\"\"", "\"");
+		}
+		else if(result.charAt(0) == '\'')
+		{
+			result = Unescape.unescape(
+				result.substring(1, result.length() - 1)
+			).replace("\'\'", "\'");
+		}
+
+		return result;
+	}
+
+	/*---------------------------------------------------------------------*/
+}
---
+++ AMICoreJava/src/main/java/net/hep/ami/utility/parser/Unescape.java
@@ -0,0 +1,103 @@
+package net.hep.ami.utility.parser;
+
+public class Unescape
+{
+	/*---------------------------------------------------------------------*/
+
+	public static String unescape(String s)
+	{
+		StringBuilder result = new StringBuilder(s.length());
+
+		/*-----------------------------------------------------------------*/
+
+		/***/ int i = 0x00000000;
+		final int l = s.length();
+
+		String code;
+		char c;
+
+		while(i < l)
+		{
+			c = s.charAt(i++);
+
+			if(c == '\\')
+			{
+				c = s.charAt(i++);
+
+				switch(c)
+				{
+					case '\\':
+						c = '\\';
+						break;
+
+					case 'b':
+						c = '\b';
+						break;
+
+					case 'f':
+						c = '\f';
+						break;
+
+					case 'n':
+						c = '\n';
+						break;
+
+					case 'r':
+						c = '\r';
+						break;
+
+					case 't':
+						c = '\t';
+						break;
+
+					case '\"':
+						c = '\"';
+						break;
+
+					case '\'':
+						c = '\'';
+						break;
+
+					case 'u':
+						/*-------------------------------------------------*/
+						/* UNICODE                                         */
+						/*-------------------------------------------------*/
+
+						if(l - i < 4)
+						{
+							c = 'u';
+							break;
+						}
+
+						code = s.substring(i + 0, i + 4); i += 4;
+
+						try
+						{ 
+							result.append(Character.toChars(Integer.parseInt(code, 16)));
+						}
+						catch(java.lang.NumberFormatException e)
+						{
+							result.append(/******************/ '?' /******************/);
+						}
+
+						continue;
+
+						/*-------------------------------------------------*/
+
+						default:
+							break;
+
+						/*-------------------------------------------------*/
+				}
+			}
+
+			result.append(c);
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return result.toString();
+	}
+
+	/*---------------------------------------------------------------------*/
+}
