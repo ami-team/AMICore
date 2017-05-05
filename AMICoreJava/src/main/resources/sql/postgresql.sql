22185704386064779592fea7b8440157f118d530
Jérôme Odier <odier.jerome@gmail.com>
2017-05-05 14:42:06 +0200

    update

---
+++ AMICoreJava/src/main/resources/sql/postgresql.sql
@@ -0,0 +1,17 @@
+------------------------------------------------------------------------------
+
+DROP TABLE `router_country_blocks_ipv6`;
+DROP TABLE `router_country_blocks_ipv4`;
+DROP TABLE `router_country_locations`;
+DROP TABLE `router_search_criteria`;
+DROP TABLE `router_search_interface`;
+DROP TABLE `router_user_role`;
+DROP TABLE `router_user`;
+DROP TABLE `router_command_role`;
+DROP TABLE `router_command`;
+DROP TABLE `router_role`;
+DROP TABLE `router_converter`;
+DROP TABLE `router_catalog`;
+DROP TABLE `router_config`;
+
+------------------------------------------------------------------------------
