8a8844e0ed96b03bb5324c470402898589b4bdbb
Jérôme Odier <odier.jerome@gmail.com>
2017-05-13 09:53:44 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/utility/Tuple6.java
@@ -0,0 +1,27 @@
+package net.hep.ami.utility;
+
+public class Tuple6<A, B, C, D, E, F>
+{
+	/*---------------------------------------------------------------------*/
+
+	public final A x;
+	public final B y;
+	public final C z;
+	public final D t;
+	public final E u;
+	public final F v;
+
+	/*---------------------------------------------------------------------*/
+
+	public Tuple6(A _x, B _y, C _z, D _t, E _u, F _v)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+		t = _t;
+		u = _u;
+		v = _v;
+	}
+
+	/*---------------------------------------------------------------------*/
+}
