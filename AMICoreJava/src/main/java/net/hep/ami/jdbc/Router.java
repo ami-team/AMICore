2c6105f3814cdf32ecf73ff7c30f5b1b7a9237b4
Jérôme Odier <odier.jerome@gmail.com>
2017-05-18 13:54:42 +0200

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/Router.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/Router.java
@@ -1,30 +1,197 @@
 package net.hep.ami.jdbc;
 
 import java.io.*;
+import java.sql.*;
 
 import net.hep.ami.*;
+import net.hep.ami.jdbc.driver.*;
+import net.hep.ami.jdbc.driver.annotation.*;
 
-public class Router extends SimpleQuerier
+public class Router implements Querier
 {
+	/*---------------------------------------------------------------------*/
+
+	private AbstractDriver m_driver;
+
 	/*---------------------------------------------------------------------*/
 
 	public Router() throws Exception
 	{
-		super("self");
+		this("self");
 	}
 
 	/*---------------------------------------------------------------------*/
 
-	public Router(String catalog) throws Exception
+	public Router(String externalCatalog) throws Exception
 	{
-		super(catalog);
+		this(
+			externalCatalog,
+			ConfigSingleton.getProperty("router"),
+			ConfigSingleton.getProperty("router_url"),
+			ConfigSingleton.getProperty("router_user"),
+			ConfigSingleton.getProperty("router_pass")
+		);
 	}
 
 	/*---------------------------------------------------------------------*/
 
 	public Router(@Nullable String externalCatalog, String internalCatalog, String jdbcUrl, String user, String pass) throws Exception
 	{
-		super(externalCatalog, internalCatalog, jdbcUrl, user, pass);
+		m_driver = DriverSingleton.getConnection(
+			externalCatalog,
+			internalCatalog,
+			jdbcUrl,
+			user,
+			pass
+		);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String mqlToSQL(String mql) throws Exception
+	{
+		return m_driver.mqlToSQL(mql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String mqlToAST(String mql) throws Exception
+	{
+		return m_driver.mqlToAST(mql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public RowSet executeQuery(String sql) throws Exception
+	{
+		return m_driver.executeQuery(sql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public RowSet executeMQLQuery(String mql) throws Exception
+	{
+		return m_driver.executeMQLQuery(mql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public int executeUpdate(String sql) throws Exception
+	{
+		return m_driver.executeUpdate(sql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public PreparedStatement sqlPrepareStatement(String sql) throws Exception
+	{
+		return m_driver.sqlPrepareStatement(sql);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public PreparedStatement sqlPrepareStatement(String sql, String[] columnNames) throws Exception
+	{
+		return m_driver.sqlPrepareStatement(sql, columnNames);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public void commit() throws Exception
+	{
+		m_driver.commit();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public void rollback() throws Exception
+	{
+		m_driver.rollback();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public void commitAndRelease() throws Exception
+	{
+		m_driver.commitAndRelease();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public void rollbackAndRelease() throws Exception
+	{
+		m_driver.rollbackAndRelease();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getInternalCatalog()
+	{
+		return m_driver.getInternalCatalog();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getExternalCatalog()
+	{
+		return m_driver.getExternalCatalog();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public Jdbc.Type getJdbcType()
+	{
+		return m_driver.getJdbcType();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getJdbcProto()
+	{
+		return m_driver.getJdbcProto();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getJdbcClass()
+	{
+		return m_driver.getJdbcClass();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getJdbcUrl()
+	{
+		return m_driver.getJdbcUrl();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getUser()
+	{
+		return m_driver.getUser();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public String getPass()
+	{
+		return m_driver.getPass();
 	}
 
 	/*---------------------------------------------------------------------*/
