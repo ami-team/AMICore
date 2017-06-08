469c48c8b99863414b025a66359305284b7184fd
Jérôme Odier <odier.jerome@gmail.com>
2017-06-09 01:18:19 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/admin/AddOrUpdateCommand.java
@@ -0,0 +1,96 @@
+package net.hep.ami.command.admin;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+import net.hep.ami.*;
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class AddOrUpdateCommand extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public AddOrUpdateCommand(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String commandName = arguments.get("command");
+		String className = arguments.get("class");
+
+		if(className == null)
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Class<?> clazz = Class.forName(className);
+
+		if((clazz.getModifiers() & Modifier.ABSTRACT) != 0x00
+		   ||
+		   ClassSingleton.extendsClass(clazz, AbstractCommand.class) == false
+		 ) {
+			throw new Exception("class '" + className + "' doesn't extend 'AbstractCommand'");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		if(commandName == null)
+		{
+			commandName = clazz.getSimpleName();
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Querier querier = getQuerier("self");
+
+		/*-----------------------------------------------------------------*/
+
+		int nb;
+
+		try
+		{
+			nb = querier.executeSQLUpdate("INSERT INTO `router_command` (`command`, `class`) VALUES (?, ?)",
+				commandName,
+				className
+			);
+		}
+		catch(Exception e)
+		{
+			nb = querier.executeSQLUpdate("UPDATE `router_command` SET `class` = ? WHERE `command` = ?",
+				className,
+				commandName
+			);
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return new StringBuilder(
+			nb > 0 ? "<info><![CDATA[done with success]]></info>"
+			       : "<error><![CDATA[nothing done]]></error>"
+		);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Add or update a command.";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "(-command=\"\")? -class=\"\"";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
---
+++ AMICoreJava/src/main/java/net/hep/ami/command/admin/RemoveCommand.java
@@ -0,0 +1,62 @@
+package net.hep.ami.command.admin;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class RemoveCommand extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public RemoveCommand(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String command = arguments.get("command");
+
+		if(command == null)
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Querier querier = getQuerier("self");
+
+		/*-----------------------------------------------------------------*/
+
+		int nb = querier.executeSQLUpdate("DELETE FROM `router_command` WHERE `command` = ?",
+			command
+		);
+
+		/*-----------------------------------------------------------------*/
+
+		return new StringBuilder(
+			nb > 0 ? "<info><![CDATA[done with success]]></info>"
+			       : "<error><![CDATA[nothing done]]></error>"
+		);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Remove a command.";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "-command=\"\"";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
