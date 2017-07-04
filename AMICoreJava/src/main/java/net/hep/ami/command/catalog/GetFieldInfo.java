4b0d9c4f29f5ce4d11afc49b8c8672711de9ceb9
Jérôme Odier <odier.jerome@gmail.com>
2017-07-04 14:49:58 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/catalog/GetFieldInfo.java
@@ -0,0 +1,85 @@
+package net.hep.ami.command.catalog;
+
+import java.util.*;
+
+import net.hep.ami.command.*;
+import net.hep.ami.jdbc.reflexion.*;
+
+public class GetFieldInfo extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public GetFieldInfo(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		StringBuilder result = new StringBuilder();
+
+		String catalog = arguments.get("catalog");
+		String entity = arguments.get("entity");
+
+		if(catalog == null
+		   ||
+		   entity == null
+		 ) {
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		result.append("<rowset>");
+
+		/*-----------------------------------------------------------------*/
+
+		for(SchemaSingleton.Column column: SchemaSingleton.getColumns(catalog, entity).values())
+		{
+			result.append(
+				"<row>"
+				+
+				"<field name=\"name\">" + column.name + "</field>"
+				+
+				"<field name=\"type\">" + column.type + "</field>"
+				+
+				"<field name=\"size\">" + column.size + "</field>"
+				+
+				"<field name=\"digits\">" + column.digits + "</field>"
+				+
+				"<field name=\"def\">" + column.def + "</field>"
+				+
+				"<field name=\"primary\">" + column.primary + "</field>"
+				+
+				"</row>"
+			);
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		result.append("</rowset>");
+
+		/*-----------------------------------------------------------------*/
+
+		return result;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "List the field info of the given catalog and entity.";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "-catalog=\"\" -entity=\"\"";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
