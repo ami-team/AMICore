4558478ee0d1d16508f2b4a3144854b8f0657080
Jérôme Odier <odier.jerome@gmail.com>
2017-08-10 16:10:41 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/utility/Tuple7.java
@@ -0,0 +1,29 @@
+package net.hep.ami.utility;
+
+public class Tuple7<A, B, C, D, E, F, G>
+{
+	/*---------------------------------------------------------------------*/
+
+	public final A x;
+	public final B y;
+	public final C z;
+	public final D t;
+	public final E u;
+	public final F v;
+	public final G w;
+
+	/*---------------------------------------------------------------------*/
+
+	public Tuple7(A _x, B _y, C _z, D _t, E _u, F _v, G _w)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+		t = _t;
+		u = _u;
+		v = _v;
+		w = _w;
+	}
+
+	/*---------------------------------------------------------------------*/
+}
