45d23d829fca6cc799698148812527a15873c0e0
Jérôme Odier <odier.jerome@gmail.com>
2017-10-02 13:35:57 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/hash/GetHashInfo.java
@@ -0,0 +1,62 @@
+package net.hep.ami.command.hash;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class GetHashInfo extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public GetHashInfo(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String hash = arguments.get("hash");
+
+		if(hash == null)
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Querier querier = getQuerier("self");
+
+		/*-----------------------------------------------------------------*/
+
+		List<Row> rowList = querier.executeSQLQuery("SELECT `json` FROM `router_user_short_url` WHERE `hash` = ? AND (`shared` = 1 OR `owner` = ?)", hash, m_AMIUser).getAll();
+
+		if(rowList.size() != 1)
+		{
+				throw new Exception("invalid hash `" + hash + "`");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return new StringBuilder().append("<rowset>")
+		                          .append("<row>")
+		                          .append("<field name=\"json\"><![CDATA[").append(rowList.get(0).getValue(0)).append("]]></field>")
+		                          .append("</row>")
+		                          .append("</rowset>")
+		;
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Get the hash information.";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
