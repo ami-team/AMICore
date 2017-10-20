13e617e865a02165579ac22cf6eb43c251f50693
Jérôme Odier <odier.jerome@gmail.com>
2017-10-20 15:38:21 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/ExtraSingleton.java
@@ -0,0 +1,69 @@
+package net.hep.ami.jdbc.reflexion;
+
+import java.util.*;
+
+public class ExtraSingleton
+{
+	/*---------------------------------------------------------------------*/
+
+	public enum Mode
+	{
+		ADD,
+		UPDATE
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static void patchFields(String catalog, String entity, List<String> fields, List<String> values, String AMIUser, Mode mode) throws Exception
+	{
+		if(fields.size() != values.size())
+		{
+			throw new Exception();
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Set<String> columnNames = SchemaSingleton.getColumnNames(catalog, entity);
+
+		/*-----------------------------------------------------------------*/
+
+		if(columnNames.contains("createdby")) {
+			patchField(fields, values, "createdby", AMIUser);
+		}
+
+		if(mode == Mode.ADD)
+		{
+			if(columnNames.contains("modifiedby")) {
+				patchField(fields, values, "modifiedby", AMIUser);
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private static void patchField(List<String> fields, List<String> values, String fieldName, String fieldValue)
+	{
+		/*-----------------------------------------------------------------*/
+
+		for(int i = 0; i < fields.size(); i++)
+		{
+			if(fields.get(i).equalsIgnoreCase(fieldName))
+			{
+				values.set(i, fieldValue);
+
+				return;
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		fields.add(fieldName);
+		values.add(fieldValue);
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+}
