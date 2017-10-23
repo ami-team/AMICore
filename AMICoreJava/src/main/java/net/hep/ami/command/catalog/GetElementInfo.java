615c4f33a6cf3e02056da4252210adabf3a86eb8
Jérôme Odier <odier.jerome@gmail.com>
2017-10-23 16:37:56 +0200

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/catalog/GetElementInfo.java
@@ -0,0 +1,61 @@
+package net.hep.ami.command.catalog;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class GetElementInfo extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public GetElementInfo(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String catalog = arguments.get("catalog");
+		String entity = arguments.get("entity");
+
+		String primaryFieldName = arguments.get("primaryFieldName");
+		String primaryFieldValue = arguments.get("primaryFieldValue");
+
+		if(catalog == null || entity == null || primaryFieldName == null || primaryFieldValue == null)
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Querier querier = getQuerier(catalog);
+
+		/*-----------------------------------------------------------------*/
+
+		RowSet queryResult = querier.executeMQLQuery(entity, "SELECT * WHERE `" + primaryFieldName.replace("`", "``") + "` = '" + primaryFieldValue.replace("'", "''") + "'");
+
+		/*-----------------------------------------------------------------*/
+
+		return queryResult.toStringBuilder();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Get the element's information.";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "-catalog=\"\" -entity=\"\" -primaryFieldName=\"\" -primaryFieldValue=\"\"";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
