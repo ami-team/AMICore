9b380982c24d16e212c7b1fc73646bc4c5a63ced
Jérôme Odier <odier.jerome@gmail.com>
2018-01-25 15:09:17 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/PathList.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/PathList.java
@@ -18,10 +18,25 @@ public class PathList
 
 	public PathList check(QId givenQId) throws Exception
 	{
+		/*-----------------------------------------------------------------*/
+
 		if(m_resolvedQId == null)
 		{
 			throw new Exception("could not resolve column name `" + givenQId + "`: not found");
 		}
+
+		/*-----------------------------------------------------------------*/
+
+		Collections.sort(m_paths, new Comparator<List<?>>() {
+
+			@Override
+			public int compare(List<?> o1, List<?> o2)
+			{
+				return o1.size() - o2.size();
+			}
+		});
+
+		/*-----------------------------------------------------------------*/
 
 		return this;
 	}
