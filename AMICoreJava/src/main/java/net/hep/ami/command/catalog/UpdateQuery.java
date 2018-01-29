2909ddf4b235c839b428d6ab9d537ec4cb5b5cf6
Jérôme Odier <odier.jerome@gmail.com>
2018-01-29 14:36:00 +0100

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/catalog/UpdateQuery.java
@@ -0,0 +1,70 @@
+package net.hep.ami.command.catalog;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+public class UpdateQuery extends AbstractCommand
+{
+	/*---------------------------------------------------------------------*/
+
+	public UpdateQuery(Map<String, String> arguments, long transactionId)
+	{
+		super(arguments, transactionId);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@Override
+	public StringBuilder main(Map<String, String> arguments) throws Exception
+	{
+		String catalog = arguments.get("catalog");
+		String entity = arguments.get("entity");
+
+		String sql = arguments.get("sql");
+		String mql = arguments.get("mql");
+
+		if(catalog == null || (sql == null && (mql == null || entity == null)))
+		{
+			throw new Exception("invalid usage");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		Querier querier = getQuerier(catalog);
+
+		/*-----------------------------------------------------------------*/
+
+		Update result;
+
+		if(sql != null)
+		{
+			result = querier.executeSQLUpdate(sql);
+		}
+		else
+		{
+			result = querier.executeMQLUpdate(entity, sql);
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return result.toStringBuilder();
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String help()
+	{
+		return "Execute a simple SQL or MQL query (update mode).";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static String usage()
+	{
+		return "-catalog=\"\" (-sql=\"\" | (-entity=\"\" -mql=\"\"))";
+	}
+
+	/*---------------------------------------------------------------------*/
+}
---
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/Update.java
@@ -0,0 +1,71 @@
+package net.hep.ami.jdbc;
+
+public class Update
+{
+	/*---------------------------------------------------------------------*/
+
+	private final int m_nbOfUpdatedRows;
+
+	private final String m_sql;
+	private final String m_mql;
+	private final String m_ast;
+
+	/*---------------------------------------------------------------------*/
+
+	public Update(int nbOfUpdatedRows) throws Exception
+	{
+		this(nbOfUpdatedRows, null, null, null);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public Update(int nbOfUpdatedRows, @Nullable String sql, @Nullable String mql, @Nullable String ast) throws Exception
+	{
+		m_nbOfUpdatedRows = nbOfUpdatedRows;
+
+		m_sql = sql != null ? sql : "";
+		m_mql = mql != null ? mql : "";
+		m_ast = ast != null ? ast : "";
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public int getNbOfUpdatedRows()
+	{
+		return m_nbOfUpdatedRows;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public String getSQL()
+	{
+		return m_sql;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public String getMQL()
+	{
+		return m_mql;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public String getAST()
+	{
+		return m_ast;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public StringBuilder toStringBuilder() throws Exception
+	{
+		return new StringBuilder().append("<sql><![CDATA[").append(m_sql).append("]]></sql>")
+		                          .append("<mql><![CDATA[").append(m_mql).append("]]></mql>")
+		                          .append("<ast><![CDATA[").append(m_ast).append("]]></ast>")
+		                          .append("<info><![CDATA[" + m_nbOfUpdatedRows + " element(s) updated with success]]></info>")
+		;
+	}
+
+	/*---------------------------------------------------------------------*/
+}
