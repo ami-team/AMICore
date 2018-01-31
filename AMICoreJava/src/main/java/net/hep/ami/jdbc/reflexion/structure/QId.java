ec6f61b65aaf7308a3fd739080505b6cc5b6bb5c
Jérôme Odier <odier.jerome@gmail.com>
2018-01-31 16:24:57 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/QId.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/QId.java
@@ -21,6 +21,10 @@ public class QId
 
 	/*---------------------------------------------------------------------*/
 
+	private final List<QId> m_qIds = new ArrayList<>();
+
+	/*---------------------------------------------------------------------*/
+
 	public QId(String qId)
 	{
 		this(qId, Deepness.COLUMN);
@@ -30,6 +34,22 @@ public class QId
 
 	public QId(String qId, Deepness deepness)
 	{
+		/*-----------------------------------------------------------------*/
+
+		int idx1 = qId.indexOf('{');
+		int idx2 = qId.indexOf('}');
+
+		if(idx1 > 0 && idx1 < idx2)
+		{
+			String[] parts = qId.substring(idx1 + 1, idx2 + 0).split(",", -1);
+
+			for(String part: parts) m_qIds.add(new QId(part));
+
+			qId = qId.substring(0, idx1);
+		}
+
+		/*-----------------------------------------------------------------*/
+
 		String[] parts = qId.split("\\.", -1);
 
 		switch(deepness)
@@ -125,9 +145,21 @@ public class QId
 
 	public QId(@Nullable String catalog, @Nullable String table, @Nullable String column)
 	{
+		this(catalog, table, column, null);
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public QId(@Nullable String catalog, @Nullable String table, @Nullable String column, @Nullable List<QId> qIds)
+	{
 		m_catalog = catalog != null ? unquote(catalog) : null;
 		m_table = table != null ? unquote(table) : null;
 		m_column = column != null ? unquote(column) : null;
+
+		if(m_qIds != null)
+		{
+			m_qIds.addAll(qIds);
+		}
 	}
 
 	/*---------------------------------------------------------------------*/
@@ -190,7 +222,7 @@ public class QId
 
 	public boolean equals(QId qId)
 	{
-		return this == qId || (this.m_catalog == qId.m_catalog && this.m_table == qId.m_table && this.m_column == qId.m_column);
+		return this == qId || (this.m_catalog == qId.m_catalog && this.m_table == qId.m_table && this.m_column == qId.m_column && this.m_qIds.equals(qId.m_qIds));
 	}
 
 	/*---------------------------------------------------------------------*/
@@ -212,6 +244,13 @@ public class QId
 	public String getColumn()
 	{
 		return m_column;
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public List<QId> getQIds()
+	{
+		return m_qIds;
 	}
 
 	/*---------------------------------------------------------------------*/
