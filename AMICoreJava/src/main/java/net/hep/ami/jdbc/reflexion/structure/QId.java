327e74330899bd2423bdb2be5d7c62d9dc235541
Jérôme Odier <odier.jerome@gmail.com>
2018-02-01 12:17:27 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/QId.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/structure/QId.java
@@ -9,137 +9,96 @@ public class QId
 
 	public enum Deepness
 	{
-		CATALOG,
-		TABLE,
-		COLUMN
+		CATALOG(1),
+		TABLE(2),
+		COLUMN(3);
+
+		protected final int length;
+
+		private Deepness(int _length)
+		{
+			length = _length;
+		}
 	}
 
 	/*---------------------------------------------------------------------*/
 
-	private final String m_catalog;
-	private final String m_table;
-	private final String m_column;
+	private final List<String> m_sids = new ArrayList<>();
+
+	private final List<QId> m_path = new ArrayList<>();
 
 	/*---------------------------------------------------------------------*/
 
-	private final List<QId> m_qIds = new ArrayList<>();
-
-	/*---------------------------------------------------------------------*/
-
-	public QId(String qId)
+	public QId(String qId) throws Exception
 	{
 		this(qId, Deepness.COLUMN);
 	}
 
 	/*---------------------------------------------------------------------*/
 
-	public QId(String qId, Deepness deepness)
+	public QId(String qId, Deepness deepness) throws Exception
 	{
 		/*-----------------------------------------------------------------*/
+
+		String tmp;
 
 		int idx1 = qId.  indexOf  ('{');
 		int idx2 = qId.lastIndexOf('}');
 
-		if(idx1 > 0 && idx1 < idx2)
+		if(idx1 > 0x00
+		   &&
+		   idx2 > idx1
+		 ) {
+			tmp = qId.substring(idx1 + 1, idx2 + 0);
+			qId = qId.substring(0x00 + 0, idx1 + 0);
+
+			for(String part: tmp.split(",", -1))
+			{
+				m_path.add(new QId(part));
+			}
+
+			int idx3 = tmp.  indexOf  ('{');
+			int idx4 = tmp.lastIndexOf('}');
+
+			if(idx3 >= 0
+			   &&
+			   idx4 >= 0
+			 ) {
+				throw new Exception("invalid QId syntax");
+			}
+		}
+		else
 		{
-			String[] parts = qId.substring(idx1 + 1, idx2 + 0).split(",", -1);
-
-			for(String part: parts) m_qIds.add(new QId(part));
-
-			qId = qId.substring(0, idx1);
+			if(idx1 >= 0
+			   ||
+			   idx2 >= 0
+			 ) {
+				throw new Exception("invalid QId syntax");
+			}
 		}
 
 		/*-----------------------------------------------------------------*/
 
 		String[] parts = qId.split("\\.", -1);
 
-		switch(deepness)
+		final int length = Math.min(
+			parts.length,
+			deepness.length
+		);
+
+		if(length > 3)
 		{
-			/*-------------------------------------------------------------*/
-
-			case CATALOG:
-				switch(parts.length)
-				{
-					case 1:
-						m_catalog = unquote(parts[0]);
-						m_table = null;
-						m_column = null;
-						break;
-
-					default:
-						m_catalog = null;
-						m_table = null;
-						m_column = null;
-						break;
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-
-			case TABLE:
-				switch(parts.length)
-				{
-					case 1:
-						m_catalog = null;
-						m_table = unquote(parts[0]);
-						m_column = null;
-						break;
-
-					case 2:
-						m_catalog = unquote(parts[0]);
-						m_table = unquote(parts[1]);
-						m_column = null;
-						break;
-
-					default:
-						m_catalog = null;
-						m_table = null;
-						m_column = null;
-						break;
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-
-			case COLUMN:
-				switch(parts.length)
-				{
-					case 1:
-						m_catalog = null;
-						m_table = null;
-						m_column = unquote(parts[0]);
-						break;
-
-					case 2:
-						m_catalog = null;
-						m_table = unquote(parts[0]);
-						m_column = unquote(parts[1]);
-						break;
-
-					case 3:
-						m_catalog = unquote(parts[0]);
-						m_table = unquote(parts[1]);
-						m_column = unquote(parts[2]);
-						break;
-
-					default:
-						m_catalog = null;
-						m_table = null;
-						m_column = null;
-						break;
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-
-			default:
-				m_catalog = null;
-				m_table = null;
-				m_column = null;
-				break;
-
-			/*-------------------------------------------------------------*/
+			throw new Exception("invalid QId syntax");
 		}
+
+		/*-----------------------------------------------------------------*/
+
+		for(	int i = 0; i < length; i++)
+		{
+			m_sids.add(unquote(parts[i]));
+		}
+
+		/*-----------------------------------------------------------------*/
 	}
 
 	/*---------------------------------------------------------------------*/
@@ -151,15 +110,22 @@ public class QId
 
 	/*---------------------------------------------------------------------*/
 
-	public QId(@Nullable String catalog, @Nullable String table, @Nullable String column, @Nullable List<QId> qIds)
+	public QId(@Nullable String catalog, @Nullable String table, @Nullable String column, @Nullable List<QId> path)
 	{
-		m_catalog = catalog != null ? unquote(catalog) : null;
-		m_table = table != null ? unquote(table) : null;
-		m_column = column != null ? unquote(column) : null;
+		if(catalog != null) {
+			m_sids.add(unquote(catalog));
+		}
 
-		if(m_qIds != null)
-		{
-			m_qIds.addAll(qIds);
+		if(catalog != null) {
+			m_sids.add(unquote(table));
+		}
+
+		if(catalog != null) {
+			m_sids.add(unquote(column));
+		}
+
+		if(m_path != null) {
+			m_path.addAll(path);
 		}
 	}
 
@@ -223,35 +189,39 @@ public class QId
 
 	public boolean equals(QId qId)
 	{
-		return this == qId || (this.m_catalog == qId.m_catalog && this.m_table == qId.m_table && this.m_column == qId.m_column && this.m_qIds.equals(qId.m_qIds));
+		return this == qId || (
+			this.m_sids.equals(qId.m_sids)
+			&&
+			this.m_path.equals(qId.m_path)
+		);
 	}
 
 	/*---------------------------------------------------------------------*/
 
 	public String getCatalog()
 	{
-		return m_catalog;
+		return (m_sids.size() > 0) ? m_sids.get(0) : null;
 	}
 
 	/*---------------------------------------------------------------------*/
 
 	public String getTable()
 	{
-		return m_table;
+		return (m_sids.size() > 1) ? m_sids.get(1) : null;
 	}
 
 	/*---------------------------------------------------------------------*/
 
 	public String getColumn()
 	{
-		return m_column;
+		return (m_sids.size() > 2) ? m_sids.get(2) : null;
 	}
 
 	/*---------------------------------------------------------------------*/
 
-	public List<QId> getQIds()
+	public List<QId> getPath()
 	{
-		return m_qIds;
+		return m_path;
 	}
 
 	/*---------------------------------------------------------------------*/
@@ -279,58 +249,19 @@ public class QId
 
 	public StringBuilder toStringBuilder(Deepness deepness)
 	{
-		List<String> list = new ArrayList<>();
+		StringBuilder result = new StringBuilder();
 
 		/*-----------------------------------------------------------------*/
 
-		switch(deepness)
-		{
-			/*-------------------------------------------------------------*/
-
-			case CATALOG:
-				if(m_catalog != null) {
-					list.add(quote(m_catalog));
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-
-			case TABLE:
-				if(m_catalog != null) {
-					list.add(quote(m_catalog));
-				}
-				if(m_table != null) {
-					list.add(quote(m_table));
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-
-			case COLUMN:
-				if(m_catalog != null) {
-					list.add(quote(m_catalog));
-				}
-				if(m_table != null) {
-					list.add(quote(m_table));
-				}
-				if(m_column != null) {
-					list.add(quote(m_column));
-				}
-				break;
-
-			/*-------------------------------------------------------------*/
-		}
+		result.append(m_sids.stream().map(str -> quote(str)).collect(Collectors.joining(".")))
+		;
 
 		/*-----------------------------------------------------------------*/
 
-		StringBuilder result = new StringBuilder(String.join(".", list));
-
-		/*-----------------------------------------------------------------*/
-
-		if(m_qIds.isEmpty() == false)
+		if(m_path.isEmpty() == false)
 		{
 			result.append("{")
-			      .append(m_qIds.stream().map(qId -> qId.toString()).collect(Collectors.joining(",")))
+			      .append(m_path.stream().map(qId -> qId.toString()).collect(Collectors.joining(",")))
 			      .append("}")
 			;
 		}
