68ecc2979c4e5db7f6b0271c0b29806d84fed65d
Jérôme Odier <odier.jerome@gmail.com>
2018-03-20 16:52:58 +0100

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/Role.java
@@ -0,0 +1,18 @@
+package net.hep.ami.command;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+
+@Target(ElementType.TYPE)
+
+public @interface Role
+{
+	/*---------------------------------------------------------------------*/
+
+	String role();
+
+	boolean secured();
+
+	/*---------------------------------------------------------------------*/
+}
