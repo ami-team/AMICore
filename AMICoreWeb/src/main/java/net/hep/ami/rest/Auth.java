4cd4067e82d7ea4bb3ee1a4953cd1dab89cbf379
Jérôme Odier <odier.jerome@gmail.com>
2018-04-17 15:33:41 +0200

    update

---
+++ AMICoreWeb/src/main/java/net/hep/ami/rest/Auth.java
@@ -0,0 +1,180 @@
+package net.hep.ami.rest;
+
+import java.util.*;
+import java.util.Map.*;
+import java.security.cert.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.servlet.http.*;
+
+import net.hep.ami.*;
+import net.hep.ami.jdbc.*;
+import net.hep.ami.utility.*;
+
+@Path("/auth")
+public class Auth
+{
+	/*---------------------------------------------------------------------*/
+
+	private static final int PASSWORD = 0;
+	private static final int CERTIFICATE = 1;
+
+	/*---------------------------------------------------------------------*/
+
+	private static final Map<String, Long> s_tokens = new AMIMap<>(AMIMap.Type.CONCURENT_HASH_MAP, false, false);
+
+	/*---------------------------------------------------------------------*/
+
+	@Context
+	private HttpServletRequest m_request;
+
+	/*---------------------------------------------------------------------*/
+
+	@POST
+	@Path("/password")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	Response password(@FormParam("username") String username, @FormParam("password") String password)
+	{
+		/*-----------------------------------------------------------------*/
+
+		return auth(username, password, PASSWORD);
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	@POST
+	@Path("/certificate")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	Response certificate()
+	{
+		/*-----------------------------------------------------------------*/
+
+		X509Certificate[] certificates = (X509Certificate[]) m_request.getAttribute("javax.servlet.request.X509Certificate");
+
+		if(certificates != null)
+		{
+			for(X509Certificate certificate: certificates)
+			{
+				if(SecuritySingleton.isProxy(certificate) == false)
+				{
+					return auth(
+						SecuritySingleton.getDN(certificate.getSubjectX500Principal()),
+						SecuritySingleton.getDN(certificate.getIssuerX500Principal()),
+						CERTIFICATE
+					);
+				}
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		return Response.status(Response.Status.FORBIDDEN).build();
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private Response auth(String X, String Y, int mode)
+	{
+		/*-----------------------------------------------------------------*/
+		/* REMOVE OLD TOKENS                                               */
+		/*-----------------------------------------------------------------*/
+
+		removeOldToken();
+
+		/*-----------------------------------------------------------------*/
+		/* CHECK USER                                                      */
+		/*-----------------------------------------------------------------*/
+
+		try
+		{
+			/*-------------------------------------------------------------*/
+
+			SimpleQuerier basicQuerier = new SimpleQuerier("self");
+
+			try
+			{
+				List<Row> row;
+
+				switch(mode)
+				{
+					case PASSWORD:
+						row = basicQuerier.executeSQLQuery("SELECT COUNT(*) FROM `router_user` WHERE `AMIUser` = ? AND `AMIPass` = ?", /*---------------------*/(X), /*---------------------*/(Y)).getAll();
+						break;
+
+					case CERTIFICATE:
+						row = basicQuerier.executeSQLQuery("SELECT COUNT(*) FROM `router_user` WHERE `clientDN` = ? AND `issuerDN` = ?", SecuritySingleton.encrypt(X), SecuritySingleton.encrypt(Y)).getAll();
+						break;
+
+					default:
+						return Response.status(Response.Status.FORBIDDEN).build();
+				}
+
+				if("1".equals(row.get(0).getValue(0)) == false)
+				{
+					return Response.status(Response.Status.FORBIDDEN).build();
+				}
+			}
+			finally
+			{
+				basicQuerier.rollbackAndRelease();
+			}
+
+			/*-------------------------------------------------------------*/
+		}
+		catch(Exception e)
+		{
+			return Response.status(Response.Status.FORBIDDEN).build();
+		}
+
+		/*-----------------------------------------------------------------*/
+		/* GET TOKEN                                                       */
+		/*-----------------------------------------------------------------*/
+
+		String result = UUID.randomUUID().toString();
+
+		s_tokens.put(result, System.currentTimeMillis());
+
+		/*-----------------------------------------------------------------*/
+
+		return Response.ok(result).build();
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private void removeOldToken()
+	{
+		List<String> oldTokens = new ArrayList<>();
+
+		/*-----------------------------------------------------------------*/
+
+		long currentTime = System.currentTimeMillis();
+
+		for(Entry<String, Long> entry: s_tokens.entrySet())
+		{
+			if((currentTime - entry.getValue()) > (2 * 60 * 60 * 1000))
+			{
+				oldTokens.add(entry.getKey());
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		for(String token: oldTokens)
+		{
+			s_tokens.remove(token);
+		}
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+}
---
+++ AMICoreWeb/src/main/java/net/hep/ami/rest/Command.java
@@ -0,0 +1,9 @@
+package net.hep.ami.rest;
+
+import javax.ws.rs.*;
+
+@Path("/command")
+public class Command
+{
+
+}
