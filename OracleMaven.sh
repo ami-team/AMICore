307c1527c85964f8f959f85701301268522e5498
Fabian Lambert <fabian.lambert@lpsc.in2p3.fr>
2018-11-15 16:01:50 +0100

    update

--- OracleMaven.sh
+++ OracleMaven.sh
@@ -7,4 +7,4 @@ curl -o $JAR ami.in2p3.fr/B681507E_E232_5951_BBBB_0DAC2BAEC223
 mvn install:install-file \
 	-DgeneratePom=true \
 	-DgroupId=com.oracle.jdbc -DartifactId=ojdbc8 \
-	-Dpackaging=jar -Dfile=$JAR -Dversion=$(unzip -p ojdbc8.jar META-INF/MANIFEST.MF | grep 'Implementation-Version:' | awk '{print $2}')
+	-Dpackaging=jar -Dfile=$JAR -Dversion=$(unzip -p ojdbc8.jar META-INF/MANIFEST.MF | awk -F ':' '/Implementation-Version/ { gsub(/[^0-9\.]/, "", $2); print $2; }')
