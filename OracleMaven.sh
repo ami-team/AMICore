fb3a70e6646fd1844cd1c85bae31bbfa1ba8c397
Odier Jérôme <odier.jerome@gmail.com>
2018-11-15 14:54:38 +0100

    update

--- OracleMaven.sh
+++ OracleMaven.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 JAR=ojdbc8.jar
-JAR_VERSION=12.2.0.1
+
+curl -o $JAR ami.in2p3.fr/B681507E_E232_5951_BBBB_0DAC2BAEC223
 
 mvn install:install-file \
 	-DgeneratePom=true \
 	-DgroupId=com.oracle.jdbc -DartifactId=ojdbc8 \
-	-Dpackaging=jar -Dfile=$JAR -Dversion=$JAR_VERSION
+	-Dpackaging=jar -Dfile=$JAR -Dversion=$(unzip -p ojdbc8.jar META-INF/MANIFEST.MF | grep 'Implementation-Version:' | awk '{print $2}')
