3600801232647c464d568b0309fcc03ba337b443
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2018-11-27 16:15:43 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/RoleSingleton.java
+++ AMICoreJava/src/main/java/net/hep/ami/RoleSingleton.java
@@ -168,7 +168,7 @@ public class RoleSingleton
 			}
 			else
 			{
-				throw new Exception("wrong role for command `" + command + "`, command's role(s) " + commandRoles + ", user's role(s) " + userRoles);
+				throw new Exception("wrong role for command `" + command + "`, expected role(s) " + commandRoles + ", found role(s) " + userRoles + ", for user `" + amiUser + "`");
 			}
 		}
 
