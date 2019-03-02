cf63429dfe03a4d9169d96c61111f75631667bad
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2019-03-02 11:10:05 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/SchemaSingleton.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/reflexion/SchemaSingleton.java
@@ -1095,7 +1095,7 @@ public class SchemaSingleton
 			result.append("<row>")
 			      .append("<field name=\"externalCatalog\"><![CDATA[").append(column.externalCatalog).append("]]></field>")
 			      .append("<field name=\"internalCatalog\"><![CDATA[").append(column.internalCatalog).append("]]></field>")
-			      .append("<field name=\"table\"><![CDATA[").append(column.table).append("]]></field>")
+			      .append("<field name=\"entity\"><![CDATA[").append(column.table).append("]]></field>")
 			      .append("<field name=\"name\"><![CDATA[").append(column.name).append("]]></field>")
 			      .append("<field name=\"type\"><![CDATA[").append(column.type).append("]]></field>")
 			      .append("<field name=\"size\"><![CDATA[").append(column.size).append("]]></field>")
