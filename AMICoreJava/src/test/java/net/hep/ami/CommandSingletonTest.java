f08032f69482a2786649adb41e1b51de797dabf6
Odier Jérôme <odier.jerome@gmail.com>
2019-03-04 16:51:45 +0100

    update

--- AMICoreJava/src/test/java/net/hep/ami/CommandSingletonTest.java
+++ AMICoreJava/src/test/java/net/hep/ami/CommandSingletonTest.java
@@ -34,9 +34,11 @@ public class CommandSingletonTest
 
 //		CommandSingleton.executeCommand("AddElement -separator=\"|\" -catalog=\"self\" -entity=\"router_catalog_extra\" -fields=\"catalog|entity|field|rank|description|webLinkScript|isHidden|isAdminOnly|isCrypted|isPrimary|isCreated|isCreatedBy|isModified|isModifiedBy|isStatable|isGroupable\" -values=\"self|router_short_url|hash|0|N/A|(\\\"\\\\\\\"\\\");|0|0|0|0|0|0|0|0|0|0\"", false);
 
-		CommandSingleton.executeCommand("RemoveElements -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -keyFields=\"FILE.name;DATASET.name;PROJECT.name\" -keyValues=\"file_;dataset_;AMI\"", false);
+		System.out.println(CommandSingleton.executeCommand("UpdateElements -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -fields=\"DATASET.name\" -values=\"dataset_\" -keyFields=\"FILE.name;DATASET.name\" -keyValues=\"file_;dataset_\"", false));
 
-/*		CommandSingleton.executeCommand("AddElement -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -fields=\"FILE.name;DATASET.name;FILE_TYPE.name\" -values=\"file_;dataset_;BINARY\"", false);
+/*		CommandSingleton.executeCommand("RemoveElements -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -keyFields=\"FILE.name;DATASET.name;PROJECT.name\" -keyValues=\"file_;dataset_;AMI\"", false);
+
+		CommandSingleton.executeCommand("AddElement -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -fields=\"FILE.name;DATASET.name;FILE_TYPE.name\" -values=\"file_;dataset_;BINARY\"", false);
 
 		CommandSingleton.executeCommand("AddElement -catalog=\"test2\" -entity=\"DATASET_FILE_BRIDGE\" -separator=\";\" -fields=\"FILE.name;DATASET.name;FILE_TYPE.name\" -values=\"file_;dataset_;A\"", false);
 
