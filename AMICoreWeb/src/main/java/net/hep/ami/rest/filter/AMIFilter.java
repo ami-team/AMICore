333775a31572e322b157f939b5b1f8521438cd92
Odier Jérôme <odier.jerome@gmail.com>
2019-06-24 17:30:38 +0200

    update

--- AMICoreWeb/src/main/java/net/hep/ami/rest/filter/AMIFilter.java
+++ AMICoreWeb/src/main/java/net/hep/ami/rest/filter/AMIFilter.java
@@ -12,7 +12,6 @@ public class AMIFilter implements ContainerResponseFilter
 	@Override
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException
 	{
-/*
 		String origin = request.getHeaderString("Origin");
 
 		if(origin != null)
@@ -23,7 +22,6 @@ public class AMIFilter implements ContainerResponseFilter
 			headers.add("Access-Control-Allow-Credentials", "true");
 			headers.add("Access-Control-Allow-Origin", origin);
 		}
-*/
 	}
 
 	/*---------------------------------------------------------------------*/
