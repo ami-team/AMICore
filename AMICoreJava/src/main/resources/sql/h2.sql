4cfbaebe5afbd219a900e3ba1d59732cec90a99a
Odier Jérôme <odier.jerome@gmail.com>
2019-12-19 16:59:43 +0100

    update

--- AMICoreJava/src/main/resources/sql/h2.sql
+++ AMICoreJava/src/main/resources/sql/h2.sql
@@ -1,3 +1,5 @@
+------------------------------------------------------------------------------------------------------------------------
+
 DROP TABLE IF EXISTS "router_ipv6_blocks";;
 DROP TABLE IF EXISTS "router_ipv4_blocks";;
 DROP TABLE IF EXISTS "router_locations";;
@@ -17,208 +19,246 @@ DROP TABLE IF EXISTS "router_entity";;
 DROP TABLE IF EXISTS "router_catalog";;
 DROP TABLE IF EXISTS "router_config";;
 
+------------------------------------------------------------------------------------------------------------------------
+
 CREATE TABLE "router_config" (
-  "id" INTEGER PRIMARY KEY
-,  "paramName" VARCHAR(128) NOT NULL
-,  "paramValue" VARCHAR(512)
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+  "id" INTEGER PRIMARY KEY,
+  "paramName" VARCHAR(128) NOT NULL,
+  "paramValue" VARCHAR(512),
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_catalog" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "externalCatalog" VARCHAR(128) NOT NULL
-,  "internalCatalog" VARCHAR(128) NOT NULL
-,  "internalSchema" VARCHAR(128)
-,  "jdbcUrl" VARCHAR(2048) NOT NULL
-,  "user" VARCHAR(128) NOT NULL
-,  "pass" VARCHAR(128) NOT NULL
-,  "json" TEXT
-,  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A'
-,  "archived" integer NOT NULL DEFAULT 0
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "externalCatalog" VARCHAR(128) NOT NULL,
+  "internalCatalog" VARCHAR(128) NOT NULL,
+  "internalSchema" VARCHAR(128),
+  "jdbcUrl" VARCHAR(2048) NOT NULL,
+  "user" VARCHAR(128) NOT NULL,
+  "pass" VARCHAR(128) NOT NULL,
+  "json" TEXT,
+  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A',
+  "archived" integer NOT NULL DEFAULT 0,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_entity" (
-   "id" INTEGER auto_increment PRIMARY KEY
-,  "catalog" VARCHAR(128) NOT NULL
-,  "entity" VARCHAR(128) NOT NULL
-,  "rank" integer
-,  "json" TEXT
-,  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A'
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+   "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "catalog" VARCHAR(128) NOT NULL,
+  "entity" VARCHAR(128) NOT NULL,
+  "rank" integer,
+  "json" TEXT,
+  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A',
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_field" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "catalog" VARCHAR(128) NOT NULL
-,  "entity" VARCHAR(128) NOT NULL
-,  "field" VARCHAR(128) NOT NULL
-,  "rank" integer
-,  "json" TEXT
-,  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A'
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "catalog" VARCHAR(128) NOT NULL,
+  "entity" VARCHAR(128) NOT NULL,
+  "field" VARCHAR(128) NOT NULL,
+  "rank" integer,
+  "json" TEXT,
+  "description" VARCHAR(512) NOT NULL DEFAULT 'N/A',
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_foreign_key" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "name" VARCHAR(128) NOT NULL
-,  "fkCatalog" VARCHAR(128) NOT NULL
-,  "fkTable" VARCHAR(128) NOT NULL
-,  "fkColumn" VARCHAR(128) NOT NULL
-,  "pkCatalog" VARCHAR(128) NOT NULL
-,  "pkTable" VARCHAR(128) NOT NULL
-,  "pkColumn" VARCHAR(128) NOT NULL
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
-,  UNIQUE ("name")
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "name" VARCHAR(128) NOT NULL,
+  "fkCatalog" VARCHAR(128) NOT NULL,
+  "fkTable" VARCHAR(128) NOT NULL,
+  "fkColumn" VARCHAR(128) NOT NULL,
+  "pkCatalog" VARCHAR(128) NOT NULL,
+  "pkTable" VARCHAR(128) NOT NULL,
+  "pkColumn" VARCHAR(128) NOT NULL,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL,
+  UNIQUE ("name")
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_converter" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "xslt" VARCHAR(128) NOT NULL
-,  "mime" VARCHAR(128) NOT NULL
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "xslt" VARCHAR(128) NOT NULL,
+  "mime" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_role" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "role" VARCHAR(128) NOT NULL
-,  "description" VARCHAR(512)
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "role" VARCHAR(128) NOT NULL,
+  "description" VARCHAR(512)
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_command" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "command" VARCHAR(128) NOT NULL
-,  "class" VARCHAR(256) NOT NULL
-,  "visible" integer NOT NULL DEFAULT 1
-,  "secured" integer NOT NULL DEFAULT 0
-,  "roleValidatorClass" VARCHAR(256)
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "command" VARCHAR(128) NOT NULL,
+  "class" VARCHAR(256) NOT NULL,
+  "visible" integer NOT NULL DEFAULT 1,
+  "secured" integer NOT NULL DEFAULT 0,
+  "roleValidatorClass" VARCHAR(256)
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_command_role" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "commandFK" integer NOT NULL
-,  "roleFK" integer NOT NULL
-,  UNIQUE  ("commandFK", "roleFK")
-,  FOREIGN KEY ("commandFK") REFERENCES "router_command" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "commandFK" integer NOT NULL,
+  "roleFK" integer NOT NULL,
+  UNIQUE  ("commandFK", "roleFK"),
+  FOREIGN KEY ("commandFK") REFERENCES "router_command" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_user" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "AMIUser" VARCHAR(128) NOT NULL
-,  "AMIPass" VARCHAR(128) NOT NULL
-,  "clientDN" VARCHAR(512)
-,  "issuerDN" VARCHAR(512)
-,  "firstName" VARCHAR(128) NOT NULL
-,  "lastName" VARCHAR(128) NOT NULL
-,  "email" VARCHAR(128) NOT NULL
-,  "country" VARCHAR(128) DEFAULT 'N/A'
-,  "ssoUser" VARCHAR(128)
-,  "json" TEXT
-,  "valid" integer NOT NULL DEFAULT 1
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "AMIUser" VARCHAR(128) NOT NULL,
+  "AMIPass" VARCHAR(128) NOT NULL,
+  "clientDN" VARCHAR(512),
+  "issuerDN" VARCHAR(512),
+  "firstName" VARCHAR(128) NOT NULL,
+  "lastName" VARCHAR(128) NOT NULL,
+  "email" VARCHAR(128) NOT NULL,
+  "country" VARCHAR(128) DEFAULT 'N/A',
+  "ssoUser" VARCHAR(128),
+  "json" TEXT,
+  "valid" integer NOT NULL DEFAULT 1,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_user_role" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "userFK" integer NOT NULL
-,  "roleFK" integer NOT NULL
-,  UNIQUE ("userFK", "roleFK")
-,  FOREIGN KEY ("userFK") REFERENCES "router_user" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "userFK" integer NOT NULL,
+  "roleFK" integer NOT NULL,
+  UNIQUE ("userFK", "roleFK"),
+  FOREIGN KEY ("userFK") REFERENCES "router_user" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_short_url" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "hash" VARCHAR(16) NOT NULL
-,  "name" VARCHAR(128) NOT NULL
-,  "rank" integer NOT NULL DEFAULT 0
-,  "json" TEXT NOT NULL
-,  "shared" integer NOT NULL DEFAULT 0
-,  "expire" integer NOT NULL DEFAULT 0
-,  "owner" VARCHAR(128) NOT NULL
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "hash" VARCHAR(16) NOT NULL,
+  "name" VARCHAR(128) NOT NULL,
+  "rank" integer NOT NULL DEFAULT 0,
+  "json" TEXT NOT NULL,
+  "shared" integer NOT NULL DEFAULT 0,
+  "expire" integer NOT NULL DEFAULT 0,
+  "owner" VARCHAR(128) NOT NULL,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_dashboard" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "control" VARCHAR(128) NOT NULL
-,  "params" TEXT NOT NULL
-,  "settings" TEXT NOT NULL
-,  "transparent" integer NOT NULL DEFAULT 0
-,  "autoRefresh" integer NOT NULL DEFAULT 1
-,  "x" integer NOT NULL DEFAULT 0
-,  "y" integer NOT NULL DEFAULT 0
-,  "width" integer NOT NULL DEFAULT 0
-,  "height" integer NOT NULL DEFAULT 0
-,  "owner" VARCHAR(128) NOT NULL
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "control" VARCHAR(128) NOT NULL,
+  "params" TEXT NOT NULL,
+  "settings" TEXT NOT NULL,
+  "transparent" integer NOT NULL DEFAULT 0,
+  "autoRefresh" integer NOT NULL DEFAULT 1,
+  "x" integer NOT NULL DEFAULT 0,
+  "y" integer NOT NULL DEFAULT 0,
+  "width" integer NOT NULL DEFAULT 0,
+  "height" integer NOT NULL DEFAULT 0,
+  "owner" VARCHAR(128) NOT NULL,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_authority" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "vo" VARCHAR(128) NOT NULL DEFAULT 'ami'
-,  "clientDN" VARCHAR(512) NOT NULL
-,  "issuerDN" VARCHAR(512) NOT NULL
-,  "notBefore" DATE NOT NULL
-,  "notAfter" DATE NOT NULL
-,  "serial" VARCHAR(128) NOT NULL
-,  "email" VARCHAR(128) NOT NULL
-,  "reason" integer
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "vo" VARCHAR(128) NOT NULL DEFAULT 'ami',
+  "clientDN" VARCHAR(512) NOT NULL,
+  "issuerDN" VARCHAR(512) NOT NULL,
+  "notBefore" DATE NOT NULL,
+  "notAfter" DATE NOT NULL,
+  "serial" VARCHAR(128) NOT NULL,
+  "email" VARCHAR(128) NOT NULL,
+  "reason" integer,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_search_interface" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "group" VARCHAR(128) NOT NULL
-,  "name" VARCHAR(128) NOT NULL
-,  "rank" integer NOT NULL DEFAULT 0
-,  "json" TEXT NOT NULL
-,  "archived" integer NOT NULL DEFAULT 0
-,  "created" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "createdBy" VARCHAR(128) NOT NULL
-,  "modified" TIMESTAMP NOT NULL DEFAULT current_timestamp
-,  "modifiedBy" VARCHAR(128) NOT NULL
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "group" VARCHAR(128) NOT NULL,
+  "name" VARCHAR(128) NOT NULL,
+  "rank" integer NOT NULL DEFAULT 0,
+  "json" TEXT NOT NULL,
+  "archived" integer NOT NULL DEFAULT 0,
+  "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "createdBy" VARCHAR(128) NOT NULL,
+  "modified" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "modifiedBy" VARCHAR(128) NOT NULL
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_locations" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "continentCode" VARCHAR(3) NOT NULL DEFAULT 'N/A'
-,  "countryCode" VARCHAR(3) NOT NULL DEFAULT 'N/A'
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "continentCode" VARCHAR(3) NOT NULL DEFAULT 'N/A',
+  "countryCode" VARCHAR(3) NOT NULL DEFAULT 'N/A'
 );;
+
+------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE "router_ipv4_blocks" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "network" VARCHAR(32) NOT NULL
-,  "rangeBegin" DECIMAL(10, 0) NOT NULL
-,  "rangeEnd" DECIMAL(10, 0) NOT NULL
-,  "geoFK" integer NOT NULL
-,  UNIQUE ("network")
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "network" VARCHAR(32) NOT NULL,
+  "rangeBegin" DECIMAL(10, 0) NOT NULL,
+  "rangeEnd" DECIMAL(10, 0) NOT NULL,
+  "geoFK" integer NOT NULL,
+  UNIQUE ("network")
 );;
 
+------------------------------------------------------------------------------------------------------------------------
+
 CREATE TABLE "router_ipv6_blocks" (
-  "id" INTEGER auto_increment PRIMARY KEY
-,  "network" VARCHAR(64) NOT NULL
-,  "rangeBegin" DECIMAL(38, 0) NOT NULL
-,  "rangeEnd" DECIMAL(38, 0) NOT NULL
-,  "geoFK" integer NOT NULL
-,  UNIQUE ("network")
+  "id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+  "network" VARCHAR(64) NOT NULL,
+  "rangeBegin" DECIMAL(38, 0) NOT NULL,
+  "rangeEnd" DECIMAL(38, 0) NOT NULL,
+  "geoFK" integer NOT NULL,
+  UNIQUE ("network")
 );;
+
+------------------------------------------------------------------------------------------------------------------------
