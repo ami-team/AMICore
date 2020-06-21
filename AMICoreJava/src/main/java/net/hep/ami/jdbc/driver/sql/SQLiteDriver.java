30fd3a1489c33ac6c2e77a9b9d75c55bef93df93
jfulach <jerome.fulachier@lpsc.in2p3.fr>
2020-06-22 00:04:36 +0200

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/driver/sql/SQLiteDriver.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/driver/sql/SQLiteDriver.java
@@ -4,6 +4,11 @@ import net.hep.ami.jdbc.driver.*;
 
 import org.jetbrains.annotations.*;
 
+import org.sqlite.Function;
+import org.sqlite.SQLiteConnection;
+
+import java.sql.SQLException;
+
 @DriverMetadata(
 	type = DriverMetadata.Type.SQL,
 	proto = "jdbc:sqlite",
@@ -25,7 +30,42 @@ public class SQLiteDriver extends AbstractDriver
 	@Override
 	public void setupSession(@NotNull String db, @NotNull String tz)
 	{
-		/* DO NOTHING */
+		try
+		{
+
+			Function.create(this.m_connection.unwrap(SQLiteConnection.class),
+					"CONCAT",
+					new Function() {
+						@Override
+						protected void xFunc() throws SQLException {
+							result(value_text(0)+value_text(1));
+						}
+					}
+			);
+		}
+		catch (Exception e)
+		{
+ 			//System.out.println("error "+ e.getMessage());
+		}
+
+		try
+		{
+
+			Function.create(this.m_connection.unwrap(SQLiteConnection.class),
+					"STDDEV",
+					new Function() {
+						@Override
+						protected void xFunc() throws SQLException {
+							result("N/A");
+						}
+					}
+			);
+		}
+		catch (Exception e)
+		{
+			//System.out.println("error "+ e.getMessage());
+		}
+
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
@@ -33,7 +73,7 @@ public class SQLiteDriver extends AbstractDriver
 	@Override
 	public String patchSQL(@NotNull String sql)
 	{
-		return sql.replaceAll("`" + this.m_internalCatalog + "`.","").replaceAll("STDDEV","MAX");
+		return sql.replaceAll("`" + this.m_internalCatalog + "`.","");
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
