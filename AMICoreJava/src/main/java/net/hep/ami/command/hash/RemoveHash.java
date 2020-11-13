47e58eaf563155af25d0777f0a91c82fd88d19ad
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2020-11-13 16:48:07 +0100

    update

---
+++ AMICoreJava/src/main/java/net/hep/ami/command/hash/RemoveHash.java
@@ -0,0 +1,77 @@
+package net.hep.ami.command.hash;
+
+import java.util.*;
+
+import net.hep.ami.jdbc.*;
+import net.hep.ami.command.*;
+
+import org.jetbrains.annotations.*;
+
+@CommandMetadata(role = "AMI_USER", visible = true, secured = false)
+public class RemoveHash extends AbstractCommand
+{
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	public RemoveHash(@NotNull Set<String> userRoles, @NotNull Map<String, String> arguments, long transactionId)
+	{
+		super(userRoles, arguments, transactionId);
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	@NotNull
+	@Override
+	public StringBuilder main(@NotNull Map<String, String> arguments) throws Exception
+	{
+		Update update;
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		String id = arguments.get("id");
+
+		if(id != null)
+		{
+			update = getQuerier("self").executeSQLUpdate("router_short_url", "DELETE FROM `router_short_url` WHERE `id` = ?0", id);
+		}
+		else
+		{
+			String hash = arguments.get("hash");
+
+			if(hash != null)
+			{
+				update = getQuerier("self").executeSQLUpdate("router_short_url", "DELETE FROM `router_short_url` WHERE `hash` = ?0", hash);
+			}
+			else
+			{
+				throw new Exception("invalid usage");
+			}
+		}
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		return new StringBuilder(
+			update.getNbOfUpdatedRows() > 0 ? "<info><![CDATA[done with success]]></info>"
+			                                : "<error><![CDATA[nothing done]]></error>"
+		);
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	@NotNull
+	@Contract(pure = true)
+	public static String help()
+	{
+		return "Remove a hash.";
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	@NotNull
+	@Contract(pure = true)
+	public static String usage()
+	{
+		return "(-id=\"\" | -hash=\"\")";
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+}
