3cf50138837e8174f6a2291e4308288a286c0ca0
Lambert <fabian.lambert@lpsc.in2p3.fr>
2020-12-14 12:58:31 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/command/AbstractProxyCommand.java
+++ AMICoreJava/src/main/java/net/hep/ami/command/AbstractProxyCommand.java
@@ -52,7 +52,7 @@ public abstract class AbstractProxyCommand extends AbstractCommand
 		/*------------------------------------------------------------------------------------------------------------*/
 
 		if("AMIAddUser".equals(command())){
-			throw new Exception(m_clientDN + " | " + m_issuerDN);
+			throw new Exception(m_clientDN + " | " + m_issuerDN + " | " +  SecuritySingleton.encrypt(m_clientDN) + " | " + SecuritySingleton.encrypt(m_issuerDN));
 		}
 
 		if(!Empty.is(m_clientDN, Empty.STRING_NULL_EMPTY_BLANK)
