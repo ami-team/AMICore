70254560580f5578311c4a60906260dd9f7a4a89
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-02-25 23:17:50 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/driver/sql/OracleDriver.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/driver/sql/OracleDriver.java
@@ -62,18 +62,58 @@ public class OracleDriver extends AbstractDriver
 
 		int cnt = 0;
 
-		if(MAJOR_VERSION >= 12)
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		int limitValue = -1;
+		int offsetValue = 0;
+
+		int flag = 0;
+
+		for(String token: tokens)
 		{
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			for(String token: tokens)
+			if("TIMESTAMP".equalsIgnoreCase(token))
 			{
-				if("TIMESTAMP".equalsIgnoreCase(token))
-				{
-					token = "TO_TIMESTAMP";
-				}
+				token = "TO_TIMESTAMP";
+			}
 
-				if(!";".equals(token))
+			if(!";".equals(token))
+			{
+				/**/ if("LIMIT".equalsIgnoreCase(token))
+				{
+					if(selectFound && !fromFound && !xxxFound)
+					{
+						result.append(" FROM dual ");
+
+						fromFound = true;
+					}
+
+					xxxFound = true;
+
+					flag = 1;
+				}
+				else if("OFFSET".equalsIgnoreCase(token))
+				{
+					flag = 2;
+				}
+				else if(flag == 1)
+				{
+					try
+					{
+						limitValue = Integer.parseInt(token);
+						flag = 0;
+					}
+					catch(NumberFormatException e) { /* IGNORE */ }
+				}
+				else if(flag == 2)
+				{
+					try
+					{
+						offsetValue = Integer.parseInt(token);
+						flag = 0;
+					}
+					catch(NumberFormatException e) { /* IGNORE */ }
+				}
+				else
 				{
 					/**/ if("(".equals(token))
 					{
@@ -97,7 +137,7 @@ public class OracleDriver extends AbstractDriver
 							{
 								fromFound = true;
 							}
-							else if("WHERE".equalsIgnoreCase(token) || "LIMIT".equalsIgnoreCase(token) || "ORDER".equalsIgnoreCase(token))
+							else if("WHERE".equalsIgnoreCase(token) || "ORDER".equalsIgnoreCase(token))
 							{
 								if(selectFound && !fromFound && !xxxFound)
 								{
@@ -125,117 +165,15 @@ public class OracleDriver extends AbstractDriver
 			}
 
 			/*--------------------------------------------------------------------------------------------------------*/
-		}
-		else
-		{
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			int limitValue = -1;
-			int offsetValue = 0;
-
-			int flag = 0;
-
-			for(String token: tokens)
-			{
-				if("TIMESTAMP".equalsIgnoreCase(token))
-				{
-					token = "TO_TIMESTAMP";
-				}
-
-				if(!";".equals(token))
-				{
-					/**/ if("LIMIT".equalsIgnoreCase(token))
-					{
-						if(selectFound && !fromFound && !xxxFound)
-						{
-							result.append(" FROM dual ");
-
-							fromFound = true;
-						}
-
-						xxxFound = true;
-
-						flag = 1;
-					}
-					else if("OFFSET".equalsIgnoreCase(token))
-					{
-						flag = 2;
-					}
-					else if(flag == 1)
-					{
-						try
-						{
-							limitValue = Integer.parseInt(token);
-							flag = 0;
-						}
-						catch(NumberFormatException e) { /* IGNORE */ }
-					}
-					else if(flag == 2)
-					{
-						try
-						{
-							offsetValue = Integer.parseInt(token);
-							flag = 0;
-						}
-						catch(NumberFormatException e) { /* IGNORE */ }
-					}
-					else
-					{
-						/**/ if("(".equals(token))
-						{
-							cnt++;
-						}
-						else if(")".equals(token))
-						{
-							cnt--;
-						}
-						else
-						{
-							if(cnt == 0)
-							{
-								/**/ if("SELECT".equalsIgnoreCase(token))
-								{
-									selectFound = true;
-									fromFound = false;
-									xxxFound = false;
-								}
-								else if("FROM".equalsIgnoreCase(token))
-								{
-									fromFound = true;
-								}
-								else if("WHERE".equalsIgnoreCase(token) || "ORDER".equalsIgnoreCase(token))
-								{
-									if(selectFound && !fromFound && !xxxFound)
-									{
-										result.append(" FROM dual ");
-
-										fromFound = true;
-									}
-
-									xxxFound = true;
-								}
-							}
-						}
-
-						result.append(Tokenizer.backQuotesToDoubleQuotes(token));
-					}
-				}
-			}
-
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			if(selectFound && !fromFound && !xxxFound)
-			{
-				result.append(" FROM dual");
-
-				fromFound = true;
-			}
-
-			/*--------------------------------------------------------------------------------------------------------*/
 
 			if(limitValue >= 0)
 			{
-				result = new StringBuilder().append("SELECT * FROM (SELECT a.*, ROWNUM AS ORACLE_ROWNUM FROM (").append(result).append(") a WHERE ROWNUM <= ").append(limitValue + offsetValue).append(") WHERE ORACLE_ROWNUM >= ").append(offsetValue + 1).append(" ORDER BY ORACLE_ROWNUM");
+				if(MAJOR_VERSION >= 12) {
+					result = new StringBuilder().append(result).append("OFFSET ").append(offsetValue).append(" ROWS FETCH NEXT ").append(limitValue).append(" ROWS ONLY");
+				}
+				else {
+					result = new StringBuilder().append("SELECT * FROM (SELECT a.*, ROWNUM AS ORACLE_ROWNUM FROM (").append(result).append(") a WHERE ROWNUM <= ").append(limitValue + offsetValue).append(") WHERE ORACLE_ROWNUM >= ").append(offsetValue + 1).append(" ORDER BY ORACLE_ROWNUM");
+				}
 			}
 
 			/*--------------------------------------------------------------------------------------------------------*/
