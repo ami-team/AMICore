424be8801695f9655d218f69d3386776a974afe1
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-08-03 16:14:32 +0200

    update

--- AMICoreJava/src/main/java/net/hep/ami/command/dashboard/UpdateWidget.java
+++ AMICoreJava/src/main/java/net/hep/ami/command/dashboard/UpdateWidget.java
@@ -23,19 +23,20 @@ public class UpdateWidget extends AbstractCommand
 	public StringBuilder main(@NotNull Map<String, String> arguments) throws Exception
 	{
 		String id = arguments.get("id");
+		String transparent = arguments.get("transparent");
 		String x = arguments.get("x");
 		String y = arguments.get("y");
 		String width = arguments.get("width");
 		String height = arguments.get("height");
 
-		if(id == null || x == null || y == null || width == null || height == null)
+		if(id == null || transparent == null || x == null || y == null || width == null || height == null)
 		{
 			throw new Exception("invalid usage");
 		}
 
 		/*-----------------------------------------------------------------*/
 
-		return getQuerier("self").executeSQLUpdate("router_dashboard", "UPDATE `router_dashboard` SET `x` = ?0, `y` = ?1, `width` = ?2, `height` = ?3 WHERE `id` = ?4", x, y, width, height, id).toStringBuilder();
+		return getQuerier("self").executeSQLUpdate("router_dashboard", "UPDATE `router_dashboard` SET `transparent` = ?0, `x` = ?1, `y` = ?2, `width` = ?3, `height` = ?4 WHERE `id` = ?5 AND `owner` = ?6", transparent, x, y, width, height, id, m_AMIUser).toStringBuilder();
 
 		/*-----------------------------------------------------------------*/
 	}
@@ -55,7 +56,7 @@ public class UpdateWidget extends AbstractCommand
 	@Contract(pure = true)
 	public static String usage()
 	{
-		return "-id=\"\" -x=\"\"-y=\"\" -width=\"\" -height=\"\"";
+		return "-id=\"\" -transparent=\"\" -x=\"\"-y=\"\" -width=\"\" -height=\"\"";
 	}
 
 	/*---------------------------------------------------------------------*/
