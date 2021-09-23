49e1e0e591990d9895c00bab5430a2b721d82654
Lambert <fabian.lambert@lpsc.in2p3.fr>
2021-09-23 15:46:08 +0200

    Update PingNode

--- AMICoreJava/src/main/java/net/hep/ami/command/server/PingNode.java
+++ AMICoreJava/src/main/java/net/hep/ami/command/server/PingNode.java
@@ -27,6 +27,7 @@ public class PingNode extends AbstractCommand
 		/*------------------------------------------------------------------------------------------------------------*/
 
 		String hostName = arguments.getOrDefault("hostName", CommandSingleton.HOSTNAME);
+		String service = arguments.getOrDefault("service", "web");
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
@@ -37,7 +38,7 @@ public class PingNode extends AbstractCommand
 		List<Row> rows = querier.executeSQLQuery("router_monitoring", "SELECT `id` FROM `router_monitoring` WHERE `node` = ?0", hostName).getAll();
 
 		Update update = (rows.size() == 1) ? querier.executeSQLUpdate("router_monitoring", "UPDATE `router_monitoring` SET `modified` = CURRENT_TIMESTAMP WHERE `id` = ?0", rows.get(0).getValue(0))
-		                                   : querier.executeSQLUpdate("router_monitoring", "INSERT INTO `router_monitoring` (`node`, `service`, `frequency`) VALUES (?0, 'web', ?1)", hostName, ConfigSingleton.getProperty("monitoring_frequency", 30))
+		                                   : querier.executeSQLUpdate("router_monitoring", "INSERT INTO `router_monitoring` (`node`, `service`, `frequency`) VALUES (?0, ?1, ?2)", hostName, service, ConfigSingleton.getProperty("monitoring_frequency", 30))
 		;
 
 		/*------------------------------------------------------------------------------------------------------------*/
