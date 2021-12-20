c52c20b84d89d5427094153ecd655142f907c643
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-12-20 13:03:57 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/SecuritySingleton.java
+++ AMICoreJava/src/main/java/net/hep/ami/SecuritySingleton.java
@@ -985,7 +985,7 @@ try{
 		return !s.isEmpty() ? new String(decrypt(org.bouncycastle.util.encoders.Base64.decode(s.getBytes(StandardCharsets.UTF_8))), StandardCharsets.UTF_8)
 		                    : ""
 		;
-		} catch(Exception e)
+} catch(Exception e)
 {
 	throw new Exception(e.getMessage() + " " + s,  e);
 }
