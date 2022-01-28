22f3996e9dff8d77a61660fbddd205d73d44b786
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-01-28 13:29:25 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/command/user/ResetPassword.java
+++ AMICoreJava/src/main/java/net/hep/ami/command/user/ResetPassword.java
@@ -53,17 +53,16 @@ public class ResetPassword extends AbstractCommand
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
-		List<Row> rowList = getQuerier("self").executeSQLQuery("router_user", "SELECT `AMIUser`, `AMIPass`, `email` FROM `router_user` WHERE `AMIUser` = ?0 AND `valid` != 0", amiLogin).getAll(10, 0);
+		List<Row> rowList = getQuerier("self").executeSQLQuery("router_user", "SELECT `AMIUser``email` FROM `router_user` WHERE `AMIUser` = ?0 AND `valid` != 0", amiLogin).getAll(10, 0);
 
 		if(rowList.size() > 0)
 		{
 			Row row = rowList.get(0);
 
 			String tmpUser = row.getValue(0);
-			String tmpPass = row.getValue(1);
-			String  email  = row.getValue(2);
+			String  email  = row.getValue(1);
 
-			String userdata = "{\"user\": \"" + Utility.escapeJavaString(tmpUser) + "\", \"old_pass\": \"" + SecuritySingleton.generateTmpPassword(tmpPass) + "\"}";
+			String userdata = "{\"user\": \"" + Utility.escapeJavaString(tmpUser) + "\", \"old_pass\": \"" + SecuritySingleton.generateTmpPassword(tmpUser) + "\"}";
 
 			userdata = new String(org.bouncycastle.util.encoders.UrlBase64.encode(userdata.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
 
