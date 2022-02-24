3f25913593029a68aae7337adb8b89d68245195b
Lambert <fabian.lambert@lpsc.in2p3.fr>
2022-02-24 11:13:17 +0100

    Changing attach/detach certificate algo

--- AMICoreJava/src/main/java/net/hep/ami/command/user/GetUserInfo.java
+++ AMICoreJava/src/main/java/net/hep/ami/command/user/GetUserInfo.java
@@ -427,7 +427,7 @@ public class GetUserInfo extends AbstractCommand
 		 ) {
 			/*--------------------------------------------------------------------------------------------------------*/
 
-			if(!querier.executeSQLQuery("router_user", "SELECT `AMIUser` FROM `router_user` WHERE `id` != ?0, `clientDN` = ?#1, `issuerDN` = ?#2", _id, bean.getClientDN(), bean.getIssuerDN()).getAll().isEmpty())
+			if(!querier.executeSQLQuery("router_user", "SELECT `AMIUser` FROM `router_user` WHERE `id` != ?0 AND `clientDN` = ?#1 AND `issuerDN` = ?#2", _id, bean.getClientDN(), bean.getIssuerDN()).getAll().isEmpty())
 			{
 				throw new Exception("this certificate is already associated to another account");
 			}
