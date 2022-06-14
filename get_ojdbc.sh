eae3f73de1fab92eba5b2bf15adbc69ae944545d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-06-14 10:16:14 +0200

    update

--- get_ojdbc.sh
+++ get_ojdbc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DRIVER=ojdbc8
+DRIVER=ojdbc10
 
-curl -o $DRIVER.jar http://ami.in2p3.fr/download/CED08B8F_B440_C42E_82DF_DFD6B6664F45
+curl -L -o $DRIVER.jar https://download.oracle.com/otn-pub/otn_software/jdbc/1914/ojdbc10.jar
 
 mvn install:install-file \
 	-DgeneratePom=true \
 	-DgroupId=com.oracle.jdbc -DartifactId=$DRIVER \
 	-Dpackaging=jar -Dfile=$DRIVER.jar -Dversion=$(unzip -p $DRIVER.jar META-INF/MANIFEST.MF | awk -F ':' '/Implementation-Version/ { gsub(/[^0-9\.]/, "", $2); print $2; }')
+
+rm -f $DRIVER.jar
