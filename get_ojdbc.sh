f4527f7db771650b527b0d94e86771e96e3d428e
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-06-14 09:28:15 +0200

    update

--- get_ojdbc.sh
+++ get_ojdbc.sh
@@ -2,7 +2,7 @@
 
 DRIVER=ojdbc8
 
-curl -o $DRIVER.jar http://ami.in2p3.fr/CED08B8F_B440_C42E_82DF_DFD6B6664F45
+curl -o $DRIVER.jar http://ami.in2p3.fr/download/CED08B8F_B440_C42E_82DF_DFD6B6664F45
 
 mvn install:install-file \
 	-DgeneratePom=true \
