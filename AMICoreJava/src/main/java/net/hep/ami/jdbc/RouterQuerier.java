05b7f7f4d6640ae44209d5c38ab7cd4404162651
Jérôme Fulachier <jerome.fulachier@lpsc.in2p3.fr>
2022-07-05 15:59:10 +0200

    sqlite: PRAGMA foreign_keys=ON

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
@@ -676,7 +676,7 @@ public class RouterQuerier implements Querier
 
 		SchemaSingleton.Table router_command = SchemaSingleton.getEntityInfo("self", "router_command");
 
-		//router_command.columns.get("visible").groupable = true;
+		router_command.columns.get("visible").groupable = true;
 		//router_command.columns.get("secured").groupable = true;
 
 		/*------------------------------------------------------------------------------------------------------------*/
