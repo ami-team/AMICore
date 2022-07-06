75513de22158f46d67c08ee759fb0bf8cdf07f5a
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-07-06 09:53:28 +0200

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
@@ -677,7 +677,6 @@ public class RouterQuerier implements Querier
 		SchemaSingleton.Table router_command = SchemaSingleton.getEntityInfo("self", "router_command");
 
 		router_command.columns.get("visible").groupable = true;
-		//router_command.columns.get("secured").groupable = true;
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
