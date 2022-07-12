fb0979501724cedef0e145332b16847bda10e79d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-07-12 13:52:00 +0200

    update

--- AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
+++ AMICoreJava/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
@@ -619,6 +619,7 @@ public class RouterQuerier implements Querier
 
 		SchemaSingleton.Table router_catalog = SchemaSingleton.getEntityInfo("self", "router_catalog");
 
+		router_catalog.columns.get("internalCatalog").webLinkScript = "import net.hep.ami.data.WebLink;\n\nwebLink = new WebLink();\n\nif(rowSet.isANameOrLabel(\"internalCatalog\"))\n{\n\twebLink.newLinkProperties().setLabel(\"Show/Edit catalog\").setHRef(\"./?subapp=schemaViewer&userdata=\" + row.getValue(\"internalCatalog\")).setTarget(\"_blank\");\n}\n\nreturn webLink;\n";
 		router_catalog.columns.get("internalCatalog").hidden = true;
 		router_catalog.columns.get("internalSchema").hidden = true;
 		router_catalog.columns.get("jdbcUrl").adminOnly = true;
