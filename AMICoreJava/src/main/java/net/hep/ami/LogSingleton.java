7616308b4d2ea310b6aab01a4c800c8eb161e9f0
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-12-08 13:24:34 +0100

    update

--- AMICoreJava/src/main/java/net/hep/ami/LogSingleton.java
+++ AMICoreJava/src/main/java/net/hep/ami/LogSingleton.java
@@ -102,7 +102,7 @@ public class LogSingleton
 		}
 
 		/*------------------------------------------------------------------------------------------------------------*/
-		/* ADD LOGBACK LOG APPENDER                                                                                   */
+		/* ADD LOGBACK APPENDER                                                                                       */
 		/*------------------------------------------------------------------------------------------------------------*/
 
 		if(ClassSingleton.extendsClass(clazz, AbstractLogAppender.class))
