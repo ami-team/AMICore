b37e208a54aa55f41f30e248e2e3142d95792e62
Fabian Lambert <fabian.lambert@lpsc.in2p3.fr>
2023-11-08 13:22:37 +0100

    version MQTT2 pour test

--- AMICoreWeb/src/main/java/net/hep/ami/servlet/FrontEnd.java
+++ AMICoreWeb/src/main/java/net/hep/ami/servlet/FrontEnd.java
@@ -46,8 +46,8 @@ public class FrontEnd extends HttpServlet
 	{
 		super.init(config);
 
-		new MQTT().init();
-		//new MQTT2().init();
+		//new MQTT().init();
+		new MQTT2().init();
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
