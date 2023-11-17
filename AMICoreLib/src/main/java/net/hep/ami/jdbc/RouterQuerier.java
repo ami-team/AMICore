5e7ce7f9b1cf226afce4b2a59703a65b3a06d800
Fabian Lambert <fabian.lambert@lpsc.in2p3.fr>
2023-11-17 10:59:01 +0100

    add catalog show admin fields

--- AMICoreLib/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
+++ AMICoreLib/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
@@ -624,8 +624,8 @@ public class RouterQuerier implements Querier
 		SchemaSingleton.Table router_catalog = SchemaSingleton.getEntityInfo("self", "router_catalog");
 
 		router_catalog.columns.get("externalCatalog").webLinkScript = "import net.hep.ami.data.WebLink;\n\nwebLink = new WebLink();\n\nif(rowSet.isANameOrLabel(\"externalCatalog\"))\n{\n\twebLink.newLinkProperties().setLabel(\"Show/Edit catalog\").setHRef(\"./?subapp=schemaViewer&userdata=\" + row.getValue(\"externalCatalog\")).setTarget(\"_blank\");\n}\n\nreturn webLink;\n";
-		router_catalog.columns.get("internalCatalog").hidden = true;
-		router_catalog.columns.get("internalSchema").hidden = true;
+		router_catalog.columns.get("internalCatalog").adminOnly = true;
+		router_catalog.columns.get("internalSchema").adminOnly = true;
 		router_catalog.columns.get("jdbcUrl").adminOnly = true;
 		router_catalog.columns.get("user").crypted = true;
 		router_catalog.columns.get("pass").crypted = true;
