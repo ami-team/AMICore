863444c1936f154aafa43e4c031ad632951f0d80
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-07-02 14:08:31 +0200

    update

--- AMICoreLib/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
+++ AMICoreLib/src/main/java/net/hep/ami/jdbc/RouterQuerier.java
@@ -706,22 +706,22 @@ public class RouterQuerier implements Querier
 		router_user.columns.get("modified").modified = true;
 
 		/*------------------------------------------------------------------------------------------------------------*/
-/*
+
 		SchemaSingleton.Table router_dashboard = SchemaSingleton.getEntityInfo("self", "router_dashboard");
 
 		router_dashboard.columns.get("owner").createdBy = true;
 		router_dashboard.columns.get("created").created = true;
 		router_dashboard.columns.get("modified").modified = true;
-*/
+
 		/*------------------------------------------------------------------------------------------------------------*/
-/*
+
 		SchemaSingleton.Table router_dashboard_controls = SchemaSingleton.getEntityInfo("self", "router_dashboard_controls");
 
 		router_dashboard_controls.columns.get("params").json = true;
 		router_dashboard_controls.columns.get("settings").json = true;
 		router_dashboard_controls.columns.get("created").created = true;
 		router_dashboard_controls.columns.get("modified").modified = true;
-*/
+
 		/*------------------------------------------------------------------------------------------------------------*/
 
 		SchemaSingleton.Table router_search_interface = SchemaSingleton.getEntityInfo("self", "router_search_interface");
