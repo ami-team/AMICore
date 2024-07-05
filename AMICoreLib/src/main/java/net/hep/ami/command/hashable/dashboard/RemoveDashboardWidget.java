92c918df08bd35055c5af7ed7f99e51b12e47325
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-07-05 20:32:14 +0200

    update

--- AMICoreLib/src/main/java/net/hep/ami/command/hashable/dashboard/RemoveDashboardWidget.java
+++ AMICoreLib/src/main/java/net/hep/ami/command/hashable/dashboard/RemoveDashboardWidget.java
@@ -1,16 +1,22 @@
 package net.hep.ami.command.hashable.dashboard;
 
-import net.hep.ami.command.AbstractCommand;
-import net.hep.ami.command.CommandMetadata;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
+import java.util.*;
 
-import java.util.Map;
-import java.util.Set;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.core.type.*;
+
+import net.hep.ami.command.*;
+import net.hep.ami.utility.*;
+
+import org.jetbrains.annotations.*;
 
 @CommandMetadata(role = "AMI_USER", visible = true)
 public class RemoveDashboardWidget extends AbstractCommand
 {
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	static TypeReference<LinkedHashMap<String, Object>> DICT_TYPE_REF = new TypeReference<>() {};
+
 	/*----------------------------------------------------------------------------------------------------------------*/
 
 	public RemoveDashboardWidget(@NotNull Set<String> userRoles, @NotNull Map<String, String> arguments, long transactionId)
@@ -24,7 +30,40 @@ public class RemoveDashboardWidget extends AbstractCommand
 	@Override
 	public StringBuilder main(@NotNull Map<String, String> arguments) throws Exception
 	{
-		return new StringBuilder("<error><![CDATA[nothing done]]></error>");
+		String hash = arguments.get("hash");
+		String widgetId = arguments.get("widgetId");
+
+		if(Empty.is(hash, Empty.STRING_NULL_EMPTY_BLANK)
+		   ||
+		   Empty.is(widgetId, Empty.STRING_NULL_EMPTY_BLANK)
+		) {
+			throw new Exception("invalid usage");
+		}
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		String dashboard = getQuerier("self").executeSQLQuery("router_dashboard", "SELECT `json` FROM `router_dashboard` WHERE `hash` = ?0 AND `owner` = ?1", hash, m_AMIUser).getAll().get(0).getValue(0);
+
+		if(Empty.is(dashboard, Empty.STRING_AMI_NULL))
+		{
+			throw new Exception("invalid dashboard");
+		}
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		Map<String, Object> dashboardJSON = new ObjectMapper().readValue(dashboard, DICT_TYPE_REF);
+
+		dashboardJSON.remove(widgetId);
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		dashboard = new ObjectMapper().writeValueAsString(dashboardJSON);
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		return getQuerier("self").executeSQLUpdate("router_dashboard", "UPDATE `router_dashboard` SET `json` = ?0 WHERE `hash` = ?1 AND `owner` = ?2", dashboard, hash, m_AMIUser).toStringBuilder();
+
+		/*------------------------------------------------------------------------------------------------------------*/
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
@@ -42,7 +81,7 @@ public class RemoveDashboardWidget extends AbstractCommand
 	@Contract(pure = true)
 	public static String usage()
 	{
-		return "(-id=\"\" | -hash=\"\") -???=\"\"";
+		return "-hash=\"\" -widgetId=\"\"";
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
