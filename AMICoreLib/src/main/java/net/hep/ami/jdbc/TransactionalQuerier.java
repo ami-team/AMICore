29921dcb210ea2c5f4e9744909e3de63847b8821
fabian.lambert@lpsc.in2p3.fr <fabian.lambert@lpsc.in2p3.fr>
2026-09-14 23:14:14 +0200

    New constructor for TransactionalQuerier debug jenkins

--- AMICoreLib/src/main/java/net/hep/ami/jdbc/TransactionalQuerier.java
+++ AMICoreLib/src/main/java/net/hep/ami/jdbc/TransactionalQuerier.java
@@ -37,12 +37,18 @@ public class TransactionalQuerier implements Querier
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 
+	/*
 	public TransactionalQuerier(@NotNull String catalog, long transactionId) throws Exception
 	{
 		String AMIUser = ConfigSingleton.getProperty("admin_user", "admin");
 		String timeZone = ConfigSingleton.getProperty("time_zone", "UTC");
 
 		m_driver = TransactionPoolSingleton.getConnection(catalog, AMIUser, timeZone, 0x00, m_transactionId = transactionId);
+	}
+	 */
+	public TransactionalQuerier(@NotNull String catalog, long transactionId) throws Exception
+	{
+		m_driver = TransactionPoolSingleton.getConnection(catalog, m_transactionId = transactionId);
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
